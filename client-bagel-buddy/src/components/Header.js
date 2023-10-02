1fc03c84a588ffa357e0a2de6f218588ff0c24f8
wendeehsu <b05705046@ntu.edu.tw>
2023-10-01 20:12:43 -0500

    feat: add header profile

--- client-bagel-buddy/src/components/Header.js
+++ client-bagel-buddy/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Header.css';
 
 function Header() {
+    const user_img_url = "../../images/user1.png";
+
     return (
         <div className="header-section">
             <div className='header-brand'>
@@ -12,6 +14,13 @@ function Header() {
                     Bagel Buddies
                 </p>
             </div>
+            <p className='header-nav'>
+                Teachers
+            </p>
+            <p className='header-nav'>
+                Lessons
+            </p>
+            <div className='header-user' style={{ backgroundImage: `url(${user_img_url})` }} />
         </div>
     )
 }
