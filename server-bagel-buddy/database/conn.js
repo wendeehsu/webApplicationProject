8733e7bc3aa06f51aaa9fc23990093f4c0ad051f
wendeehsu <b05705046@ntu.edu.tw>
2023-10-23 21:44:50 -0500

    feat: setup db connection

---
+++ server-bagel-buddy/database/conn.js
@@ -0,0 +1,23 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://user1:user1@cluster0.ujikdi9.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+exports.pingDB = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
--- server-bagel-buddy/server.js
+++ server-bagel-buddy/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const { pingDB } = require('./database/conn.js');
 const app = express();
 
 app.use(express.static(path.join(__dirname,"..","client-bagel-buddy", "build")))
 
-app.use(require('./express-server'));
+// app.use(require('./express-server'));
+pingDB();
 
 app.get("*", async (req, res) => {
     res.sendFile(path.join(__dirname,"..","client-bagel-buddy", "build", "index.html"))
