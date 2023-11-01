ee6b03956d0ee82fe797d8311109e07a0274cc5d
hazelkd <hdunnling@comcast.net>
2023-10-31 21:20:46 -0500

    Task 18 finished, submitting

--- client-bagel-buddy/src/containers/student/teacherList/teacherDetail.js
+++ client-bagel-buddy/src/containers/student/teacherList/teacherDetail.js
@@ -3,27 +3,29 @@ import { useParams } from 'react-router';
 import { useNavigate } from 'react-router-dom';
 import './teacherDetail.css';
 
+
 function TeacherDetailPage() {
     const { id } = useParams();
     const navigate = useNavigate();
     const [reviewList, setReviewList] = useState([]);
+    const skills = [2, 0]
     useEffect(() => {
         setReviewList([{
-            id:1,
+            id: 1,
             name: "Jacob Jones",
             nationality: "Singapore",
             imgUrl: "1",
             star: 4,
             comment: "The English teacher in question is an exceptional educator who has left an indelible mark on both my academic and personal growth. Their dedication to fostering a love for literature and language is nothing short of inspiring."
-        },{
-            id:2,
+        }, {
+            id: 2,
             name: "Jerome Bell",
             nationality: "Australia",
             imgUrl: "2",
             star: 3,
             comment: "The English teacher in question is an exceptional educator who has left an indelible mark on both my academic and personal growth. Their dedication to fostering a love for literature and language is nothing short of inspiring."
-        },{
-            id:3,
+        }, {
+            id: 3,
             name: "Akita Yagor",
             nationality: "Japan",
             imgUrl: "3",
@@ -42,21 +44,55 @@ function TeacherDetailPage() {
                     alt="back icon" />
                 <h2>Back</h2>
             </div>
-            
-            <div>
-                {/* TODO: teacher info */}
+
+            <div className='teacher-info'>
+                <img
+                    className='teacher-img'
+                    src='../../../images/user1.png' />
+                <div className='star-chunk'>
+                    <h1 className='teacher-name'> Alysa Yang</h1>
+                    <p className='teacher-lang'> Spanish </p>
+                    <div className='starList'>
+                        {
+                            Array.from({ length: 5 }) /*not a number before*/
+                                .map((item, index) => (
+                                    <img
+                                        key={`star-${index}`}
+                                        className='star'
+                                        src="../../images/starSolid.png" />
+                                ))
+                        }
+                    </div>
+
+                    <div className='skill-list'>
+                        {
+                            ["Writing", "Reading", "Speaking", "Grammar"]
+                                .filter((skill, index) => skills.includes(index))
+                                .map((skill, index) => (
+                                    <div className='skill-chip' key={`skill-${index}`}>
+                                        <p className='skillBox'>{skill}</p>
+                                    </div>
+                                ))
+                        }
+                    </div>
+                </div>
+
+                <div className='teacher-text'>
+                    <p> Hello, I'm thrilled to introduce myself as your child's teacher for this academic year. My name is Alysa Yang, and I am truly excited to embark on this educational journey with your child and you. Education is a partnership between teachers, students, and parents, and I believe in open communication and collaboration to ensure the best possible learning experience for your child.</p>
+                </div>
+
             </div>
 
             <div className='teacher-detail-lower-section'>
                 <div className='teacher-review-section'>
                     <h1>Review</h1>
                     <div className='review'>
-                        { reviewList.map((review, index) => (
+                        {reviewList.map((review, index) => (
                             <div key={`review-${index}`}>
                                 <div className='review-header'>
                                     <div
                                         className='review-img'
-                                        style={{backgroundImage: `url(../../images/user${index+1}.png)`}}
+                                        style={{ backgroundImage: `url(../../images/user${index + 1}.png)` }}
                                     />
                                     <h2>{review.name}</h2>
                                     <p className='review-nationality'>{review.nationality}</p>
